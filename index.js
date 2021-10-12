833a5d204d1b6c52b45dc9cd5f980b29368a217f
Your Name <you@example.com>
2021-10-12 12:41:55 +0530

    First Deploy

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 mongoose
 .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() =>
