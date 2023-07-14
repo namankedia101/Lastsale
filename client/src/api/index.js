eab50be1058bd47c1d4ef4afa624a2e3c517b6af
Your Name <you@example.com>
2023-07-14 14:07:44 +0530

    separated backend and frontend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL:"https://lastsale.onrender.com"});
+const API = axios.create({baseURL:"http://localhost:5000"});
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
       req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
